51a430badda2fd3932e35f3f33e93fff65af3810
Joao Paulo Furtado <joaopaulofurtado@live.com>
2022-01-04 13:55:13 -0800

    first commit

---
+++ compile.ts
@@ -0,0 +1,16 @@
+import fs from "fs";
+import path from "path";
+import solc from "solc";
+
+const __dirname = path.resolve();
+
+for (let i = 2; i < process.argv.length; i++) {
+  console.log(`⚙️ Compiling ${process.argv[i]} contract...`);
+
+  const inboxPath = path.resolve(__dirname, "contracts", process.argv[i]);
+  const source = fs.readFileSync(inboxPath, "utf8");
+
+  const result = solc.compile(source, 1); // 1 is the number of contracts to compile
+
+  console.log(result);
+}
